67b788418129ebb785de9383f5357630b6b69393
Eyob <eabebe91@gmail.com>
2022-08-21 13:16:05 +0300

    cors policy solved

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 require("dotenv/config");
 const authRouter = require("./routes/auth");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(cors());
 
 // Import Routes
 app.use("/auth", authRouter);
